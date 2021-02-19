0db60b7845a67bd507bdafb79341f0d45da7efa9
mvjl000 <milosz.devmail@gmail.com>
2021-02-20 00:19:21 +0100

    Prepare files for development

---
+++ src/views/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './Root.styles';
+
+const Root: React.FC = () => {
+  return (
+    <Wrapper>
+      <h1>SOCNET</h1>
+    </Wrapper>
+  );
+};
+
+export default Root;
