01b8a77932a66f676790749620e8d60e0aecd4e5
mvjl000 <milosz.devmail@gmail.com>
2021-03-05 15:17:31 +0100

    New Post request to backend

--- src/views/AddPost/AddPost.tsx
+++ src/views/AddPost/AddPost.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Wrapper,
   Heading,
@@ -6,18 +6,62 @@ import {
   TextAreaContainer,
 } from './AddPost.styles';
 import { AddPostButton } from 'shared/components/AddPostButton';
+import axios from 'axios';
+
+interface NewPost {
+  title: string;
+  description: string;
+}
 
 const AddPost: React.FC = () => {
+  const [newPostContent, setNewPostContent] = useState<NewPost>({
+    title: '',
+    description: '',
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) =>
+    setNewPostContent({
+      ...newPostContent,
+      [event.target.name]: event.target.value,
+    });
+
+  const handleAddNewPost = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (
+      newPostContent.title.length < 100 &&
+      newPostContent.description.length > 3 &&
+      newPostContent.description.length < 2000
+    ) {
+      axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/newPost`, {
+        title: newPostContent.title,
+        description: newPostContent.description,
+      });
+    }
+  };
+
   return (
     <Wrapper>
       <Heading>New Post</Heading>
-      <StyledForm>
+      <StyledForm onSubmit={handleAddNewPost}>
         <label htmlFor=''>
-          Title <input placeholder='This is optional*' type='text' />
+          Title{' '}
+          <input
+            placeholder='This is optional*'
+            type='text'
+            name='title'
+            value={newPostContent.title}
+            onChange={handleInputChange}
+          />
         </label>
         <TextAreaContainer>
           <p>Content</p>
-          <textarea></textarea>
+          <textarea
+            name='description'
+            value={newPostContent.description}
+            onChange={handleInputChange}
+          ></textarea>
         </TextAreaContainer>
         <AddPostButton>Add Post</AddPostButton>
       </StyledForm>
