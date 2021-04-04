16edea6b1ed2e36148b13039f4d096be461f4f67
mvjl000 <milosz.devmail@gmail.com>
2021-04-04 21:06:38 +0200

    Create req to backend for dynamic searching users

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { Input } from './SearchBar.styles';
 
 interface SearchBarProps {
@@ -8,10 +9,31 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ closeDropDown }) => {
     const [barValue, setBarValue] = useState('');
+    const [searchResults, setSearchResults] = useState<string[]>([]);
 
     const history = useHistory();
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setBarValue(event.target.value);
+    useEffect(() => {
+        if (barValue) {
+            const reqData = async () => {
+                try {
+                    const responseData = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/searchUsers`, {
+                        searchValue: barValue
+                    });
+                    setSearchResults(responseData.data.users);
+                } catch (err) {
+                    console.log(err.response.data.message);
+                };
+            };
+            reqData();
+        } else {
+            setSearchResults([]);
+        }
+    }, [barValue]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setBarValue(event.target.value);
+    };
 
     const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -23,9 +45,16 @@ const SearchBar: React.FC<SearchBarProps> = ({ closeDropDown }) => {
     };
 
     return (
+        <>
         <form onSubmit={handleSearch}>
         <Input placeholder='Search for person' value={barValue} onChange={handleInputChange}/>
         </form>
+        {searchResults && (
+            <ul>
+                {searchResults.map(username => <li key={username}>{username}</li>)}
+            </ul>
+        )}
+        </>
     )
 }
 
