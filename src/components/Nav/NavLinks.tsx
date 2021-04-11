946cb99a91e86c6454fc0730552043c95cd38e7a
mvjl000 <milosz.devmail@gmail.com>
2021-04-11 18:51:14 +0200

    Add admin page to navlinks

--- src/components/Nav/NavLinks.tsx
+++ src/components/Nav/NavLinks.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthContext from 'shared/context/auth-context';
 import { useToggleSearchBar } from 'hooks/useToggleSearchBar';
 import { StyledNavLinks } from './NavLinks.styles';
 import PersonIcon from '@material-ui/icons/Person';
@@ -14,6 +15,7 @@ interface NavLinksProps {
 
 const NavLinks: React.FC<NavLinksProps> = ({ closeDropDown, handleLogout, username }) => {
   const { isSearchBarVisible } = useToggleSearchBar(true);
+  const auth = useContext(AuthContext);
 
   const handleLogoutClick = () => {
   if (closeDropDown) {
@@ -26,6 +28,11 @@ const NavLinks: React.FC<NavLinksProps> = ({ closeDropDown, handleLogout, userna
     <StyledNavLinks>
       {isSearchBarVisible && <li>
         <SearchBar closeDropDown={closeDropDown}/>
+      </li>}
+      {auth.userData![0] === process.env.REACT_APP_ADMIN_ID && <li>
+        <NavLink to='/admin' onClick={closeDropDown}>
+          Admin Page
+        </NavLink>
       </li>}
       <li>
         <NavLink to={`/profile/${username}`} onClick={closeDropDown}>
