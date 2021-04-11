8ebd0de5583e86f1bfc6109ce17b7eeeb0273bfa
mvjl000 <milosz.devmail@gmail.com>
2021-04-11 19:09:16 +0200

    Get reported posts req to backend

--- src/views/AdminPage/AdminPage.tsx
+++ src/views/AdminPage/AdminPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import AuthContext from 'shared/context/auth-context';
 import { Wrapper, Heading } from './AdminPage.styles';
+import { PostType } from 'types/posts-types';
 
 const AdminPage: React.FC = () => {
+    const [posts, setPosts] = useState<PostType[]>([]);
+    const auth = useContext(AuthContext);
+
+    useEffect(() => {
+        const reqData = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/posts/reportedPosts`, {
+              headers: {
+                Authorization: `Bearer ${auth.token}`,
+              },
+            })
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }    
+        };
+        reqData();
+    }, []);
+
     return (
         <Wrapper>
             <Heading>Reported Posts</Heading>
