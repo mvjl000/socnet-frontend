63c99b59ebe765b8b8fe995cfbd3889979cc5a14
mvjl000 <milosz.devmail@gmail.com>
2021-04-13 19:55:33 +0200

    Create AboutPage

--- src/views/About/About.styles.ts
+++ src/views/About/About.styles.ts
@@ -1,1 +1,69 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 100%;
+`;
+
+export const Heading = styled.h1`
+    margin-bottom: 50px;
+    font-size: 50px;
+    font-weight: 400;
+    text-align: center;
+    @media (min-width: 1024px) {
+        text-align: left;
+        margin: 0 auto 50px auto;
+        width: 90%;
+    }
+`;
+
+export const ContentWrapper = styled.div`
+    margin: auto;
+    width: 90%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-column-gap: 30px;
+    @media (min-width: 1024px) {
+        grid-template-columns: 2fr 4fr 2fr;
+    }
+`;
+
+export const Article = styled.article`
+    margin: 50px 0;
+    width: 80%;
+    max-width: 600px;
+    justify-self: center;
+    &:nth-child(2) {
+        @media (min-width: 1024px) {
+            padding: 0 100px;
+            border-left: 2px solid #ddd;
+            border-right: 2px solid #ddd;
+        }
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 28px;
+    font-weight: 300;
+    text-align: center;
+`;
+
+export const Content = styled.p`
+    padding: 0 10px;
+    color: #333;
+    font-weight: 300;
+    font-size: 19px;
+    text-align: left;
+
+    a {
+        color: inherit;
+        transition: .2s;
+        &:hover {
+            color: #ff4257;
+        }
+    }
+
+    svg {
+        color: #111;
+        transform: translate(5px, 3px);
+    }
+`;
--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Wrapper, Heading, ContentWrapper, Article, Title, Content } from './About.styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const About: React.FC = () => {
     return (
-        <h1>
-            About Socnet
-        </h1>
+        <Wrapper>
+            <Heading>
+                About Socnet
+            </Heading>
+            <ContentWrapper>
+                <Article>
+                    <Title>What Is Socnet?</Title>
+                    <Content>It is a fullstack social media app built for study purpose. Allows to create account, add posts, interact with other users by commenting their activity.
+                        <br/>
+                        <br/>
+                    In the future I would like to add some new features, for example live chat and stuff like that.
+                    </Content>
+                </Article>
+                <Article>
+                    <Title>Tech stack</Title>
+                    <Content>As I said in the first article, this is a Fullstack App - let me start with frontend.
+                        <br/>
+                        <br/>
+                        For this project chose Javascript framework - React (CRA) combined with Typescript, styled components and some third party solutions for animations etc.
+                        <br/>
+                        <br/>
+                        On the backend I used NodeJS with Express framework. For storing data - MongoDB, NoSQL database.
+                    </Content>
+                </Article>
+                <Article>
+                    <Title>Contanct</Title>
+                    <Content>To see more interesting projects, necessarily check my GitHub account - <a href="https://github.com/mvjl000">mvjl000</a><GitHubIcon/>
+                    <br/>
+                    <br/>
+                    Here is my email adress: milosz.devmail@gmail.com
+                    <br/>
+                    <br/>
+                    I highly recommend to see <a href="https://github.com/mvjl000">Socnet Repos</a>, look for socnet-frontend and socnet-backend
+                    <br/>
+                    <br/>
+                    Made By Milosz Piskadlo
+                    </Content>
+                </Article>
+            </ContentWrapper>
+        </Wrapper>
     )
 }
 
