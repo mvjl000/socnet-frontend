e10f9db663af266e02f6adb18d9ec56da8c6dbbb
mvjl000 <milosz.devmail@gmail.com>
2021-04-13 18:29:36 +0200

    Create about route

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -15,6 +15,7 @@ import Profile from 'views/Profile/Profile';
 import AddPost from 'views/AddPost/AddPost';
 import CommentPage from 'views/CommentPage/CommentPage';
 import AdminPage from 'views/AdminPage/AdminPage';
+import About from 'views/About/About';
 import { Wrapper } from './Root.styles';
 import { useAuth } from 'hooks/useAuth';
 import { useScreenInfo } from 'hooks/useScreenInfo';
@@ -31,6 +32,7 @@ const Root: React.FC = () => {
         <Route path='/new-post' component={AddPost} />
         <Route path='/post/:postId' component={CommentPage} />
         <Route path='/profile/:uname' component={Profile} />
+        <Route path='/about' component={About}/>
         {userData![0] === process.env.REACT_APP_ADMIN_ID ? (
           <ReportsProvider>
             <Route path='/admin' component={AdminPage}/>
@@ -43,6 +45,7 @@ const Root: React.FC = () => {
     routes = (
       <Switch>
         <Route path='/' component={Login} exact />
+        <Route path='/about' component={About}/>
         <Redirect to='/' />
       </Switch>
     );
