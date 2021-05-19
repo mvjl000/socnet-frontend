2a4abb074f1c8390d3fc4f80855d822fcc5bbdbd
mvjl000 <milosz.devmail@gmail.com>
2021-05-19 23:33:05 +0200

    Add hiding results dropdown after unfocusing searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { Input, ResultsContainer, Wrapper, ListItem } from './SearchBar.styles';
@@ -8,62 +8,93 @@ interface SearchBarProps {
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ closeDropDown }) => {
-    const [barValue, setBarValue] = useState('');
-    const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [barValue, setBarValue] = useState('');
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [isSearchBarFocused, setIsSearchBarFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-        if (barValue) {
-            const reqData = async () => {
-                try {
-                    const responseData = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/searchUsers`, {
-                        searchValue: barValue
-                    });
-                    setSearchResults(responseData.data.users);
-                } catch (err) {
-                    console.log(err.response.data.message);
-                };
-            };
-            reqData();
-        } else {
-            setSearchResults([]);
+  useEffect(() => {
+    if (barValue) {
+      const reqData = async () => {
+        try {
+          const responseData = await axios.post(
+            `${process.env.REACT_APP_BACKEND_URL}/user/searchUsers`,
+            {
+              searchValue: barValue,
+            }
+          );
+          setSearchResults(responseData.data.users);
+        } catch (err) {
+          console.log(err.response.data.message);
         }
-    }, [barValue]);
+      };
+      reqData();
+    } else {
+      setSearchResults([]);
+    }
+  }, [barValue]);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBarValue(event.target.value);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBarValue(event.target.value);
+  };
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    history.push(`/profile/${barValue}`);
+    setBarValue('');
+    if (closeDropDown) {
+      closeDropDown();
+    }
+  };
+
+  const handleUserClick = () => {
+    if (closeDropDown) {
+      closeDropDown();
+    }
+    setBarValue('');
+    setSearchResults([]);
+  };
+
+  useEffect(() => {
+    const isInputFocused = () => {
+      setIsSearchBarFocused(document.activeElement === inputRef.current);
     };
 
-    const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        history.push(`/profile/${barValue}`);
-        setBarValue('');
-        if (closeDropDown) {
-            closeDropDown();
-        }
+    window.addEventListener('click', isInputFocused);
+
+    return () => {
+      window.removeEventListener('click', isInputFocused);
     };
+  }, []);
 
-    const handleUserClick = () => {
-        if (closeDropDown) {
-            closeDropDown();
-        };
-        setBarValue('');
-        setSearchResults([]);
-    };
+  return (
+    <Wrapper>
+      <form onSubmit={handleSearch}>
+        <Input
+          placeholder="Search for person"
+          value={barValue}
+          onChange={handleInputChange}
+          isListActive={searchResults.length > 0}
+          ref={inputRef}
+        />
+      </form>
+      {searchResults.length > 0 && isSearchBarFocused && (
+        <ResultsContainer isListActive={searchResults.length > 0}>
+          {searchResults.map((username) => (
+            <ListItem
+              to={`/profile/${username}`}
+              onClick={handleUserClick}
+              key={username}
+            >
+              {username}
+            </ListItem>
+          ))}
+        </ResultsContainer>
+      )}
+    </Wrapper>
+  );
+};
 
-    return (
-        <Wrapper>
-        <form onSubmit={handleSearch}>
-        <Input placeholder='Search for person' value={barValue} onChange={handleInputChange} isListActive={searchResults.length > 0}/>
-        </form>
-        {searchResults.length > 0 && (
-            <ResultsContainer isListActive={searchResults.length > 0}>
-                {searchResults.map(username => <ListItem to={`/profile/${username}`} onClick={handleUserClick} key={username}>{username}</ListItem>)}
-            </ResultsContainer>
-        )}
-        </Wrapper>
-    )
-}
-
-export default SearchBar
+export default SearchBar;
