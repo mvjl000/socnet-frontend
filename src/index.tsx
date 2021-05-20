e038087201602ccb4236f09165080b2333ce7ae3
mvjl000 <milosz.devmail@gmail.com>
2021-05-20 23:19:26 +0200

    Add store provider, implement posts store in main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'store/store';
 import Root from 'views/Root';
 import 'assets/styles/fonts.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Provider store={store}>
+      <Root />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useContext, useState } from 'react';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'store/reducers/rootReducer';
+import { PostsStateTypes } from 'store/reducers/postsReducer';
 import { PostsContext } from 'shared/context/postsProvider';
 import AuthContext from 'shared/context/auth-context';
 import Post from 'components/Post/Post';
 import Loader from 'shared/components/Loader';
 import AboutButton from 'shared/components/AboutButton';
 import { Wrapper, Heading } from './Main.styles';
+import { setFetchedPosts } from 'store/actions';
 
 const Main: React.FC = () => {
+  const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
-  const { posts, setFetchedPosts } = useContext(PostsContext);
+  // const { posts, setFetchedPosts } = useContext(PostsContext);
+  const posts = useSelector<RootState, PostsStateTypes['posts']>(
+    (state) => state.posts.posts
+  );
   const auth = useContext(AuthContext);
 
   useEffect(() => {
@@ -19,7 +27,7 @@ const Main: React.FC = () => {
         `${process.env.REACT_APP_BACKEND_URL}/posts/getAllPosts`
       );
       setIsLoading(false);
-      setFetchedPosts(response.data.posts);
+      dispatch(setFetchedPosts(response.data.posts));
     };
     reqData();
   }, [setFetchedPosts]);
