6e999849b3db34665d7486e431bba7183049f36e
mvjl000 <milosz.devmail@gmail.com>
2021-05-21 00:03:23 +0200

    Edit post action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,7 +3,8 @@ import { PostType } from 'types/posts-types';
 export type Action =
   | { type: 'SET_FETCHED_POSTS'; payload: { posts: PostType[] } }
   | { type: 'ADD_POST'; payload: { post: PostType } }
-  | { type: 'DELETE_POST'; payload: { postId: string } };
+  | { type: 'DELETE_POST'; payload: { postId: string } }
+  | { type: 'EDIT_POST'; payload: { postId: string; content: string } };
 
 export const setFetchedPosts = (posts: PostType[]): Action => ({
   type: 'SET_FETCHED_POSTS',
@@ -18,4 +19,9 @@ export const addPost = (post: PostType): Action => ({
 export const deletePost = (postId: string): Action => ({
   type: 'DELETE_POST',
   payload: { postId },
+});
+
+export const editPost = (postId: string, content: string): Action => ({
+  type: 'EDIT_POST',
+  payload: { postId, content },
 });
