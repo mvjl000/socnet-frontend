de98e0132999b00f4cb9b1c91c1935fe6922e63d
mvjl000 <milosz.devmail@gmail.com>
2021-05-21 00:17:18 +0200

    Create like post action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -4,7 +4,15 @@ export type Action =
   | { type: 'SET_FETCHED_POSTS'; payload: { posts: PostType[] } }
   | { type: 'ADD_POST'; payload: { post: PostType } }
   | { type: 'DELETE_POST'; payload: { postId: string } }
-  | { type: 'EDIT_POST'; payload: { postId: string; content: string } };
+  | { type: 'EDIT_POST'; payload: { postId: string; content: string } }
+  | {
+      type: 'LIKE_POST';
+      payload: {
+        postId: string;
+        userId: string;
+        actionType: 'LIKE' | 'DISLIKE';
+      };
+    };
 
 export const setFetchedPosts = (posts: PostType[]): Action => ({
   type: 'SET_FETCHED_POSTS',
@@ -24,4 +32,13 @@ export const deletePost = (postId: string): Action => ({
 export const editPost = (postId: string, content: string): Action => ({
   type: 'EDIT_POST',
   payload: { postId, content },
+});
+
+export const likePost = (
+  postId: string,
+  userId: string,
+  actionType: 'LIKE' | 'DISLIKE'
+): Action => ({
+  type: 'LIKE_POST',
+  payload: { postId, userId, actionType },
 });
