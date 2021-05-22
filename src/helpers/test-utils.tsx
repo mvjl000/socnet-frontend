478eee11ca2646db10f5211664a2da500f4faa57
mvjl000 <milosz.devmail@gmail.com>
2021-05-22 22:55:41 +0200

    Create test utils file

---
+++ src/helpers/test-utils.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from 'store/store';
+
+const AllTheProviders: FC = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
