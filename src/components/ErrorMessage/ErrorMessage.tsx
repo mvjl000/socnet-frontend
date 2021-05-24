3a59b195e50c5e1f7962e6adb6e7f273182fa6ae
mvjl000 <milosz.devmail@gmail.com>
2021-05-24 19:46:59 +0200

    Add default error message

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,7 +4,9 @@ interface ErrorMessageProps {
   message: string;
 }
 
-const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+const ErrorMessage: React.FC<ErrorMessageProps> = ({
+  message = 'Something went wrong, please try again later.',
+}) => {
   return (
     <Wrapper>
       <h1>Oops!</h1>
