4fe73b26a45739bb037427f3c58535bb29163193
mvjl000 <milosz.devmail@gmail.com>
2021-05-24 20:53:30 +0200

    Create error context provider

---
+++ src/context/errorProvider.tsx
@@ -0,0 +1,27 @@
+import { createContext, useCallback, useState } from 'react';
+
+interface ErrorContextProps {
+  errorMessage: string | null;
+  dispatchError: (message: string) => void;
+}
+
+export const ErrorContext = createContext<ErrorContextProps>({
+  errorMessage: null,
+  dispatchError: () => {},
+});
+
+const ErrorProvider: React.FC = ({ children }) => {
+  const [error, setError] = useState<string | null>(null);
+
+  const dispatchError = useCallback((message) => {
+    setError(message);
+  }, []);
+
+  return (
+    <ErrorContext.Provider value={{ errorMessage: error, dispatchError }}>
+      {children}
+    </ErrorContext.Provider>
+  );
+};
+
+export default ErrorContext;
