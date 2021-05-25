f263ad43b40b98083d3424993168d3dab90a931a
mvjl000 <milosz.devmail@gmail.com>
2021-05-25 18:22:16 +0200

    Create timeout for error modal

--- src/hooks/useError.ts
+++ src/hooks/useError.ts
@@ -1,12 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ErrorContext } from 'context/errorProvider';
 
 export const useError = () => {
   const errorContext = useContext(ErrorContext);
+  const { errorMessage, dispatchError } = errorContext;
 
   if (!errorContext) {
     throw Error('useError needs to be used inside ErrorContext');
   }
+
+  useEffect(() => {
+    if (!errorMessage) return;
+
+    const timer = setTimeout(() => {
+      dispatchError('');
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [errorMessage]);
 
   return errorContext;
 };
